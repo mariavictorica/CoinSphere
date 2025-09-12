fa7c0c25fe44e9f009ea1c823ea754c79f3da016
emvic <emvictoricag@gmail.com>
2025-09-11 22:39:25 -0600

    Agregamos MainScreen con título CoinSphere

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -1,50 +1,40 @@
 package com.example.coinsphere
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.coinsphere.ui.theme.CoinSphereTheme
+import androidx.compose.ui.unit.dp
+import com.example.coinsphere.ui.theme.*
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CoinSphereTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  CoinSphereScreen()
-                }
-            }
-        }
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "CoinSphere",
+            style = MaterialTheme.typography.headlineMedium,
+            color = TextMain
+        )
     }
 }
 
-
-
+@Preview
 @Composable
-fun CoinSphereScreen() {
-    var selectedTab by remember { mutableStateOf("Global Market Cap") }
-    var searchText by remember { mutableStateOf("") }
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     CoinSphereTheme {
-        CoinSphereScreen()
+        MainScreen()
     }
+
 }
+
+
+
