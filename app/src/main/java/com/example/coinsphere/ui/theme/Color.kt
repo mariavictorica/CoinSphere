c8c71206393bd5ca2e733cd873b8d260873f808b
emvic <emvictoricag@gmail.com>
2025-09-11 22:11:21 -0600

    Configuramos el fondo obscuro en los archivos color, theme y type

--- app/src/main/java/com/example/coinsphere/ui/theme/Color.kt
+++ app/src/main/java/com/example/coinsphere/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.coinsphere.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Background = Color(0xFF0B1020)   // Fondo principal
+val Surface    = Color(0xFF151B2E)   // Tarjetas / superficies
+val TextMain   = Color(0xFFE8ECF8)   // Texto principal
+val TextDim    = Color(0xFF9AA3B2)   // Texto secundario
+val AccentBlue = Color(0xFF2979FF)   // Azul de acento
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
